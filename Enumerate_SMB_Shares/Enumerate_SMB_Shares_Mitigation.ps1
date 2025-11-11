7dea0a08eb4c9b65c5dfcb5688311f100a24d5e4
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-11 12:14:54 -0500

    Get Item Property

--- Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
+++ Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
@@ -4,6 +4,9 @@ if(Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'){
 Write-Host "Route Founded..."
 Set-Location 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
 Get-ChildItem
+Get-ItemProperty -Name RestrictAnonymous
+Set-ItemProperty 
+
 }
 else{
 Write-Warning "Route not founded..."
