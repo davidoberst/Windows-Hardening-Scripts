c05ccc2f789221bc3006ec5ff3b5280a39f972d8
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-11 14:18:44 -0500

    Enumerate SMB Shares (Mitigation)

--- Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
+++ Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
@@ -1,12 +1,14 @@
-﻿#Enumerate SMB Shares Mitigation
+﻿#Enumerate SMB Shares (Mitigation)
 #EXECUTE AS AN ADMIN!!!
 if(Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'){
 Write-Host "Route Founded..."
 Set-Location 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
 Get-ChildItem
 Get-ItemProperty -Name RestrictAnonymous
-Set-ItemProperty 
-
+Set-ItemProperty -Value '1'
+gpduptadte / force
+Start-Sleep -Seconds1
+Write-Warning "Value Changed!"
 }
 else{
 Write-Warning "Route not founded!"
