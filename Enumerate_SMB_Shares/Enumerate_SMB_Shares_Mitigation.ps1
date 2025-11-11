36ca9c78d1397afcb37d61de29e4e77ebb99ce73
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-11 11:21:59 -0500

    Enumerate SMB Shares Mitigation

--- Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
+++ Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
@@ -1,4 +1,5 @@
-﻿
+﻿#Enumerate SMB Shares Mitigation
+#EXECUTE AS AN ADMIN!!!
 if(Test-Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'){
 Write-Host "Route Founded..."
 Set-Location 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa'
