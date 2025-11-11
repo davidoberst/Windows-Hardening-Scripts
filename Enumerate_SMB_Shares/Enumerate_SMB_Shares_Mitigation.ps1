e9b3459cef6e59baef1c375e763724a40f8cf708
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-10 19:08:51 -0500

    Enumerate SMB Shares

---
+++ Enumerate_SMB_Shares/Enumerate_SMB_Shares_Mitigation.ps1
@@ -0,0 +1,5 @@
+﻿#Anonymous SAM Account and Share Enumeration (Mitigation)
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'RestrictAnonymous' -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'RestrictAnonymousSAM' -Value 1 -Type DWord -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters' -Name 'RestrictNullSessAccess' -Value 1 -Type DWord -Force
