5176526c02dcda3f650ea545480e1d7fe952efd4
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-13 14:19:20 -0500

    CVE-2021-36942.

--- NTLM_Incoming_traffic_not_restricted/CVE‑2021‑36942.ps1
+++ NTLM_Incoming_traffic_not_restricted/CVE‑2021‑36942.ps1
@@ -1,9 +1,11 @@
-﻿#NTLM Incoming traffic not restricted
+#NTLM Incoming traffic not restricted (Mitigation)
+#Disable NTLM on any AD CS Servers in your domain using the group policy Network security: Restrict NTLM: Incoming NTLM traffic.
+#CVE-2021-36942
 $path = 'HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\MSV1_0'
-Test-Path $path
-if($path){
- Write-Output "founded"
+if(Test-Path $path){
+  Get-ChildItem
+  Set-ItemProperty -Name 'RestrictReceivingNTLMTraffic' -Value '2' 
+}else{
+Write-Warning "PATH NOT FOUNDED."
 }
-
-
 
